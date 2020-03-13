8a2018adf6e70cd9885865e86ff0b4e16e281391
gabriel-roque <gabrielrms.dll@gmail.com>
2020-03-13 15:22:17 -0300

    feat: rename names and labels

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ import NumberFormat from 'react-number-format';
 import Error from './Error';
 
 function App() {
-  const { register, handleSubmit, reset, errors, control } = useForm();
+  const { register, handleSubmit, errors, control } = useForm();
 
   const onSubmit = data => {
     console.log(data);
-    reset();
+    alert('check your console');
   };
 
   useEffect(() => {
@@ -23,57 +23,57 @@ function App() {
   }, [errors]);
 
   return (
-    <div className='content'>
+    <div className="content">
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className='form-group'>
-          <label htmlFor='produto'>Nome do Produto: </label>
+        <div className="form-group">
+          <label htmlFor="product">Name of Product: </label>
           <input
-            type='text'
-            name='produto'
-            id='produto'
+            type="text"
+            name="product"
+            id="product"
             ref={register({ ...nomeInput })}
           />
-          <Error errors={errors} name='produto' />
+          <Error errors={errors} name="product" />
         </div>
 
-        <div className='form-group'>
-          <label>Quantidade: </label>
+        <div className="form-group">
+          <label>Quantility: </label>
           <input
-            type='number'
-            name='quantidade'
+            type="number"
+            name="quantility"
             ref={register({ ...numberInput })}
           />
-          <Error errors={errors} name='quantidade' />
+          <Error errors={errors} name="quantility" />
         </div>
 
-        <div className='form-group'>
-          <label htmlFor='cpf'>CPF: </label>
+        <div className="form-group">
+          <label htmlFor="cpf">CPF: </label>
 
           {/* Use lib 'react-input-mask' when using 'react-hook-form' 
           for STATIC MASKS */}
 
-          <InputMask mask='999.999.999-99'>
-            <input type='text' name='cpf' id='cpf' ref={register} />
+          <InputMask mask="999.999.999-99">
+            <input type="text" name="cpf" id="cpf" ref={register} />
           </InputMask>
-          <Error errors={errors} name='cpf' />
+          <Error errors={errors} name="cpf" />
         </div>
 
-        <div className='form-group'>
-          <label htmlFor='valor'>Valor do produto: </label>
+        <div className="form-group">
+          <label htmlFor="price">Price of product: </label>
 
           {/* Use lib 'react-number-format' when using 'react-hook-form' 
           for DYNAMIC INPUTS (Ex: monetary input)*/}
 
           <Controller
-            name='valor'
+            name="price"
             control={control}
             rules={{ ...requiredInput }}
             as={<NumberFormat thousandSeparator={true} prefix={'R$ '} />}
           />
-          <Error errors={errors} name='valor' />
+          <Error errors={errors} name="price" />
         </div>
-        <div className='form-group'>
-          <button type='submit'>Submit</button>
+        <div className="form-group">
+          <button type="submit">Submit</button>
         </div>
       </form>
     </div>
